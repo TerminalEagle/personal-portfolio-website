3c7842beb581dd4a5dfd7be89b47c0cc5ba683dc
TerminalEagle <affan.shaikh2309@gmail.com>
2023-11-21 16:23:01 +0530

    build fixes

--- app/api/projects/[slug]/route.ts
+++ app/api/projects/[slug]/route.ts
@@ -1,4 +1,31 @@
-import { Project } from "../route";
+import mongoose, { Schema, Model } from "mongoose";
+
+mongoose.connect("mongodb+srv://portfolio:3234n7hKctuGLx7p@datacluster.fptcmom.mongodb.net/portfolio?retryWrites=true&w=majority");
+const projectSchema = new Schema({
+  slug: String,
+  title: String,
+  description: String,
+  skills: [String],
+  technologies: [String],
+  visitWebsiteLink: String,
+  solutionLink: String,
+  projectBackground: String,
+});
+
+// Define the Project model interface
+interface IProject extends Document {
+  slug: string;
+  title: string;
+  description: string;
+  skills: string[];
+  technologies: string[];
+  visitWebsiteLink: string;
+  solutionLink: string;
+  projectBackground: string;
+}
+
+// Check if the model already exists before defining it
+const Project: Model<IProject> = mongoose.models.Project || mongoose.model<IProject>("Project", projectSchema);
 
 export async function GET(request: Request, { params }: { params: { slug: string } }) {
   const project = await Project.find({ slug: params.slug });
